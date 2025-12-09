aa4254faf8afc76167a3cc1dfff890757d6043f2
Monika_kote20 <monikakote35@gmail.com>
2025-12-09 11:16:38 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,53 @@
+import withPWA from 'next-pwa';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  async headers() {
+    return [
+      {
+        source: '/sw.js',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, must-revalidate',
+          },
+          {
+            key: 'Service-Worker-Allowed',
+            value: '/',
+          },
+        ],
+      },
+      {
+        source: '/manifest.json',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+// Check if the environment is development
+const isDev = process.env.NODE_ENV === 'development';
+
+// Conditionally wrap the config with PWA only in production
+const pwaConfig = {
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: isDev, // Disable PWA in development
+};
+
+export default withPWA(pwaConfig)(nextConfig);
